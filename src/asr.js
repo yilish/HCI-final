6be503b920328b9d334ac8555f0b3f2882faff7d
BigPearrrr <1029347073@qq.com>
2021-06-19 20:01:50 +0800

    Now clicking canvas could make it record

--- src/asr.js
+++ src/asr.js
@@ -197,7 +197,67 @@ function changeColor() {
 
 
 var i = 0;
+const constraints = {audio: true};
+if (navigator.mediaDevices.getUserMedia) {
 
+    navigator.mediaDevices.getUserMedia(constraints).then(
+        stream => {
+
+            console.log("授权成功！");
+            const recordBtn = document.body;
+            const mediaRecorder = new MediaRecorder(stream);
+
+            recordBtn.onclick = () => {
+                console.log('1');
+
+                if (mediaRecorder.state === "recording") {
+                    mediaRecorder.stop();
+                    // annyang.abort();
+                    //recordBtn.textContent = "record";
+                    console.log("录音结束");
+                } else {
+                    mediaRecorder.start();
+                    annyang.resume();
+                    console.log("录音中...");
+                    //recordBtn.textContent = "stop";
+                }
+                console.log("录音器状态：", mediaRecorder.state);
+            };
+
+            const chunks = [];
+            mediaRecorder.ondataavailable = function (e) {
+                chunks.push(e.data);
+                console.log('data')
+                console.log(e.data);
+            };
+
+            mediaRecorder.onstop = e => {
+                var blob = new Blob(chunks, {type: "audio/wav; codecs=opus"});
+                console.log(blob.size)
+                console.log('dasdsadjasoidjo')
+                console.log(blob)
+                // console.log(blob.arrayBuffer());
+                // chunks = [];
+                var audioURL = window.URL.createObjectURL(blob);
+                var audio = document.getElementById('aud');
+                audio.src = audioURL;
+                // console.log(audioURL);
+                // console.log(audioURL);
+                var reader = new FileReader();
+                reader.onloadend = () => {
+
+                }
+
+                reader.readAsDataURL(blob);
+            };
+        },
+        () => {
+            console.error("授权失败！");
+        }
+    );
+} else {
+    console.error("浏览器不支持 getUserMedia");
+}
 function testCanvasOnClick() {
     try {
         if (i++ % 2 === 0) {
